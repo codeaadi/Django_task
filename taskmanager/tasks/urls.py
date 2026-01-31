ee1c20ef42eb902792ca9326d01c8b58e183e70c
Aditya Dubey <adityadubey362@gmail.com>
2026-02-01 00:30:34 +0530

    Aditya:Implemented a common template for more active look

--- taskmanager/tasks/urls.py
+++ taskmanager/tasks/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('',task_list,name='task_list'),
      path('create/', task_create, name='task_create'),
     path('login/',auth_views.LoginView.as_view(template_name='tasks/login.html'),name='login'),
-    path('logout/',auth_views.LoginView.as_view(),name='logout'),
+    path('logout/',auth_views.LoginView.as_view(next_page='login'),name='logout'),
     path('signup/',signup,name='signup'),
   path('update/<int:pk>/', task_update, name='task_update'),
     path('delete/<int:pk>/',task_update, name='task_delete'),
