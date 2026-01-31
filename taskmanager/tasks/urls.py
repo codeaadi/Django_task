23b9e6b81270f3bbc41678d9ac2ae34d1c654632
Aditya Dubey <adityadubey362@gmail.com>
2026-02-01 00:21:57 +0530

    Aditya:Implemente all the CRUD opetation

--- taskmanager/tasks/urls.py
+++ taskmanager/tasks/urls.py
@@ -1,19 +1,13 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import signup
+from .views import signup, task_list, task_create, task_update,task_update
 
 urlpatterns = [
-    path(
-        'login/',
-        auth_views.LoginView.as_view(template_name='tasks/login.html'),
-        name='login'
-    ),
-    path(
-        'logout/',
-        auth_views.LoginView.as_view(),
-        name='logout'
-    ),
-    path(
-        'signup/',signup,name='signup'
-    )
+    path('',task_list,name='task_list'),
+     path('create/', task_create, name='task_create'),
+    path('login/',auth_views.LoginView.as_view(template_name='tasks/login.html'),name='login'),
+    path('logout/',auth_views.LoginView.as_view(),name='logout'),
+    path('signup/',signup,name='signup'),
+  path('update/<int:pk>/', task_update, name='task_update'),
+    path('delete/<int:pk>/',task_update, name='task_delete'),
 ]
